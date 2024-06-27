cb7ad0dc24724fdfa851f64ec61a557d5e9abd79
mace2831 <aaronjmaciag@gmail.com>
2024-06-26 22:17:25 -0700

    Updated navbar styles

--- Weekend Athletics/app_server/controllers/mma.js
+++ Weekend Athletics/app_server/controllers/mma.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var blogs = JSON.parse(fs.readFileSync('./data/blogs.json','utf8'));
+
 /*Get MMA view*/
 const mma = (req, res) => {
-    res.render('mma', {title: "MMA"});
+    res.render('mma', {title: "MMA", blogs});
 };
 
 module.exports = {
